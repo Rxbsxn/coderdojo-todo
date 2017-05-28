5bf9b539d191bf72c1afea008575c51c48b430eb
Robert Harężlak <harezlak.robert7@gmail.com>
2017-05-28 14:36:40 +0200

    route config by read model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.peekAll('todo'); //Use peekAll when model is local and findAll when in storage
+  }
 });
