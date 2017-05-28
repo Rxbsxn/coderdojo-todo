47de64a3f8d439ea0f2fce9afcd06408ece0ffde
Robert Harężlak <harezlak.robert7@gmail.com>
2017-05-28 14:48:06 +0200

    configure component and main template

--- app/components/todo-list/component.js
+++ app/components/todo-list/component.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  todosOrder: ['done', 'name'],
+  todosSorted: Ember.computed.sort('todos', 'todosOrder'),
+
+  action: {
+    addTodo() {
+      let newTodo = this.get("newTodoText");
+
+      this.get('store').createRecord('todo', {name: newTodo, done: false});
+      this.set("newTodoText", '');
+    }
+  }
 });
